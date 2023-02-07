1e20f19f7ba75d841b59b476dc2638ff00744925
unknown <matteospinelli10@gmail.com>
2023-02-07 11:47:44 +0100

    feat: add exercise files

--- exercise.js
+++ exercise.js
@@ -1,9 +1,20 @@
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+const isLogged = true;
+const promise = new Promise((resolve, reject) => {
+    isLogged ? 
+        resolve(Math.random()) : reject("Not logged")
+})
+
+function checkNum(num){
+    return new Promise((resolve, reject) => {
+        if (num > 0.5){
+            resolve({name: "John", age: 24})
+        } else{
+            reject("error")
+        }
+    })
 }
 
-const developer = new Person("Mario", "Rossi");
-console.log(`${developer.firstName} ${developer.lastName}`);
+promise
+    .then(checkNum)
+    .then((val) => console.log(val))
+    .catch((error) => console.log(error))
